4dd351c88f5c775752b9f0987344d5c5d53bcdd8
sagargaigol07 <41671594+sagargaigol07@users.noreply.github.com>
2020-06-18 12:15:38 +0530

    database added

--- src/main/java/com/websystique/springmvc/dao/ConnectionDao.java
+++ src/main/java/com/websystique/springmvc/dao/ConnectionDao.java
@@ -15,7 +15,7 @@ public class ConnectionDao {
 	try {
 		Class.forName("com.mysql.jdbc.Driver");  
 		con=DriverManager.getConnection(  
-		"jdbc:mysql://awsrds.cuaofirbrh2o.us-west-2.rds.amazonaws.com:3306/user","username","password");
+		"jdbc:pysql://java-application.cqgqd1oplv07.ap-south-1.rds.amazonaws.com:5432/user","postgres","javawebapp");
 		}catch(Exception e){ System.out.println(e);}
 	return con;  
 	}
